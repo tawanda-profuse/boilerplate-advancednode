cc5cb5da55e1d5b2c57823fa07c2dfa7be3b28e1
Andrew Msengezi <xrthemc@gmail.com>
2021-10-20 10:28:50 +0200

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.route('/').get((req, res) => {
-  res.render(process.cwd() + '/views/pug');
+  res.render(process.cwd() + '/views/pug/index', {title: 'Hello', message: 'Please login'});
 });
 
 const PORT = process.env.PORT || 3000;
