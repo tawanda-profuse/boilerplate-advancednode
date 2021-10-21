213f1c032754457917576919b5ebb2558687b8e3
Andrew Msengezi <xrthemc@gmail.com>
2021-10-21 11:59:26 +0200

    Update server.js
    Adding the object with a property of username in the /profile get route

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ myDB(async client => {
   });
 
   app.route('/profile').get(ensureAuthenticated, (req, res) => {
-    res.render(process.cwd() + '/views/pug/profile');
+    res.render(process.cwd() + '/views/pug/profile', { username: req.user.username });
   });
 
   // Serialization and deserialization here...
