481d03a221d310a97c8872a9960f980b5ffba739
Andrew Msengezi <xrthemc@gmail.com>
2021-10-21 10:38:28 +0200

    Update server.js
    Implement the Serialization of a Passport User

--- server.js
+++ server.js
@@ -3,15 +3,20 @@ require('dotenv').config();
 const express = require('express');
 const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
+
 const session = require('express-session');
 const passport = require('passport'); 
 const ObjectID = require('mongodb').ObjectID;
 const app = express();
 
 fccTesting(app); //For FCC testing purposes
+
 app.set('view engine', 'pug');
+
 app.use('/public', express.static(process.cwd() + '/public'));
+
 app.use(express.json());
+
 app.use(express.urlencoded({ extended: true }));
 
 app.use(session({
@@ -24,18 +29,35 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.route('/').get((req, res) => {
-  res.render(process.cwd() + '/views/pug/index', {title: 'Hello', message: 'Please login'});
-});
+// app.route('/').get((req, res) => {
+//   res.render(process.cwd() + '/views/pug/index', {title: 'Hello', message: 'Please login'});
+// });
 
-passport.serializeUser((user, done) => {
+myDB(async client => {
+  const myDataBase = await client.db('database').collection('users');
+
+  app.route('/').get((req, res) => {
+    res.render('pug', {
+      title: 'Connected to Database',
+      message: 'Please login'
+    });
+  });
+  
+  // Serialization and deserialization here...
+  passport.serializeUser((user, done) => {
   done(null, user._id);
 });
 
 passport.deserializeUser((id, done) => {
-  // myDataBase.findOne({_id: new ObjectID(id) }, (err, doc) => {
-    done(null, null);
-  // });
+  myDataBase.findOne({_id: new ObjectID(id) }, (err, doc) => {
+    done(null, doc);
+  });
+});
+
+}).catch(e => {
+  app.route('/').get((req, res) => {
+    res.render('pug', {title: e, message: 'Unable to login'});
+  });
 });
 
 const PORT = process.env.PORT || 3000;
