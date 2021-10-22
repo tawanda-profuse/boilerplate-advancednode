ce04a7ac7149e8e2152b5878957762d1ca91bef4
Andrew Msengezi <xrthemc@gmail.com>
2021-10-22 09:11:41 +0200

    Update server.js
    Adding the logout route which redirects to the Home page

--- server.js
+++ server.js
@@ -54,6 +54,15 @@ myDB(async client => {
     res.render(process.cwd() + '/views/pug/profile', { username: req.user.username });
   });
 
+   app.route('/logout').get((req, res) => {
+    req.logout();
+    res.redirect('/');
+  });
+
+  app.use((req, res, next) => {
+    res.status(404).type('text').send('Not Found');
+  });
+
   // Serialization and deserialization here...
 passport.serializeUser((user, done) => {
   done(null, user._id);
@@ -83,7 +92,6 @@ passport.use(new LocalStrategy(
   });
 });
 
-// New middleware
 function ensureAuthenticated(req, res, next) {
   if (req.isAuthenticated()) {
     return next();
