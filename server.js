c8fe13ffef0f93fa53fca644622d51cf0a1c360b
Andrew Msengezi <xrthemc@gmail.com>
2021-10-22 11:20:31 +0200

    Update server.js

--- server.js
+++ server.js
@@ -7,8 +7,11 @@ const session = require('express-session');
 const passport = require('passport');
 const routes = require('./routes');
 const auth = require('./auth.js');
-
 const app = express();
+
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
 app.set('view engine', 'pug');
 
 fccTesting(app); // For fCC testing purposes
@@ -31,6 +34,11 @@ myDB(async (client) => {
 
   routes(app, myDataBase);
   auth(app, myDataBase);
+
+  io.on('connection', (socket) => {
+    console.log('A user has connected');
+  });
+  
 }).catch((e) => {
   app.route('/').get((req, res) => {
     res.render('pug', { title: e, message: 'Unable to login' });
@@ -38,6 +46,6 @@ myDB(async (client) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+http.listen(PORT, () => {
   console.log('Listening on port ' + PORT);
 });
