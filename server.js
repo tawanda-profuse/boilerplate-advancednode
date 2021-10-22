3358b925ab9d9de116344a320fbdaad175258081
Andrew Msengezi <xrthemc@gmail.com>
2021-10-22 09:17:07 +0200

    Update server.js
    Registration of new users

--- server.js
+++ server.js
@@ -3,92 +3,96 @@ require('dotenv').config();
 const express = require('express');
 const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
-
 const session = require('express-session');
-const passport = require('passport'); 
+const passport = require('passport');
 const ObjectID = require('mongodb').ObjectID;
-const app = express();
-
 const LocalStrategy = require('passport-local');
 
-fccTesting(app); //For FCC testing purposes
-
+const app = express();
 app.set('view engine', 'pug');
 
+fccTesting(app); // For fCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
-
 app.use(express.json());
-
 app.use(express.urlencoded({ extended: true }));
 
 app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: true,
   saveUninitialized: true,
-  cookie: { secure: false}
+  cookie: { secure: false }
 }));
 
 app.use(passport.initialize());
 app.use(passport.session());
 
-// app.route('/').get((req, res) => {
-//   res.render(process.cwd() + '/views/pug/index', {title: 'Hello', message: 'Please login'});
-// });
-
-myDB(async client => {
+myDB(async (client) => {
   const myDataBase = await client.db('database').collection('users');
 
+  // Be sure to change the title
   app.route('/').get((req, res) => {
-    res.render('pug', {
-      title: 'Connected to Database',
-      message: 'Please login',
-      showLogin: true
-    });
+    // Change the response to render the Pug template
+    res.render('pug', { title: 'Connected to Database', message: 'Please login', showLogin: true, showRegistration: true });
   });
-  
   app.route('/login').post(passport.authenticate('local', { failureRedirect: '/' }), (req, res) => {
     res.redirect('/profile');
   });
-
   app.route('/profile').get(ensureAuthenticated, (req, res) => {
     res.render(process.cwd() + '/views/pug/profile', { username: req.user.username });
   });
-
-   app.route('/logout').get((req, res) => {
+  app.route('/logout').get((req, res) => {
     req.logout();
     res.redirect('/');
   });
 
+  app.route('/register').post(
+    (req, res, next) => {
+      myDataBase.findOne({ username: req.body.username }, function (err, user) {
+        if (err) {
+          next(err);
+        } else if (user) {
+          res.redirect('/');
+        } else {
+          myDataBase.insertOne({ username: req.body.username, password: req.body.password }, (err, doc) => {
+            if (err) {
+              res.redirect('/');
+            } else {
+              next(null, doc.ops[0]);
+            }
+          });
+        }
+      });
+    },
+    passport.authenticate('local', { failureRedirect: '/' }),
+    (req, res, next) => {
+      res.redirect('/profile');
+    }
+  );
   app.use((req, res, next) => {
     res.status(404).type('text').send('Not Found');
   });
-
-  // Serialization and deserialization here...
-passport.serializeUser((user, done) => {
-  done(null, user._id);
-});
-
-passport.deserializeUser((id, done) => {
-  myDataBase.findOne({_id: new ObjectID(id) }, (err, doc) => {
-    done(null, doc);
+  passport.serializeUser((user, done) => {
+    done(null, user._id);
   });
-});
-
-passport.use(new LocalStrategy(
-  function(username, password, done) {
-    myDataBase.findOne({ username: username }, function (err, user) {
-      console.log('User '+ username +' attempted to log in.');
-      if (err) { return done(err); }
-      if (!user) { return done(null, false); }
-      if (password !== user.password) { return done(null, false); }
-      return done(null, user);
+  passport.deserializeUser((id, done) => {
+    myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
+      if (err) return console.error(err);
+      done(null, doc);
     });
-  }
-));
-
-}).catch(e => {
+  });
+  passport.use(new LocalStrategy(
+    function (username, password, done) {
+      myDataBase.findOne({ username: username }, function (err, user) {
+        if (err) { return done(err); }
+        if (!user) { return done(null, false); }
+        if (password !== user.password) { return done(null, false); }
+        return done(null, user);
+      });
+    }
+  ));
+}).catch((e) => {
   app.route('/').get((req, res) => {
-    res.render('pug', {title: e, message: 'Unable to login'});
+    res.render('pug', { title: e, message: 'Unable to login' });
   });
 });
 
